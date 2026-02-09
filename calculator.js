c7ddb299e9c31a7720bf66c44dd61aa52c153e4f
pattern-screamer <schuurmanlukas@gmail.com>
2026-02-09 14:57:40 +0100

    Create high-level math function

--- calculator.js
+++ calculator.js
@@ -1,14 +1,41 @@
-const sum = function (...operands) {
-  return operands.reduce((acc, operand) => acc + operand, 0);
-}
-const subtract = function (...operands) {
-  return operands.reduce((acc, operand) => acc - operand, 0);
+//Operations
+const add = function (num1, num2) {
+  return num1 + num2;
 }
 
-const multiply = function (...operands) {
-  return operands.reduce((acc, operand) => acc * operand, 0);
+const subtract = function (num1, num2) {
+  return num1 - num2;
 }
 
-const divide = function (...operands) {
-  return operands.reduce((acc, operand) => acc / operand, 0);
+const multiply = function (num1, num2) {
+  return num1 * num2;
+}
+
+const divide = function (num1, num2) {
+  return num1 / num2;
+}
+
+const operate = function (num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
+}
+
+//Extract relevant data from mathematical calculation of syntax "(operand-one) (operator) (operand-two)"
+const extractCalculation = function(string) {
+  const stringArray = string.split(' ');
+  const operationArray = [Number(stringArray[0]), Number(stringArray[2]), stringArray[1]];
+  return operationArray;
+}
+
+//Front-end functions
+const calculate = function (string) {
+  return operate(...extractCalculation(string));
 }
--- script.js
+++
@@ -1,9 +0,0 @@
-console.log(sum(6, 6));
-console.log(subtract(6, 6));
-console.log(multiply(6, 6));
-console.log(divide(6, 6));
-
-console.log(sum(6, 6, 6));
-console.log(subtract(6, 6, 6));
-console.log(multiply(6, 6, 6));
-console.log(divide(6, 6, 6));
