17c1d3c0321f35344b4ded2c8792a95fdd8fe232
pattern-screamer <schuurmanlukas@gmail.com>
2026-02-09 15:46:38 +0100

    Develop input handling one tenth of the way

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
+const handleDigitClick = function (event) {
+  switch (event.target.id) {
+    case 'zero':
+      handleDigitInput(0);
+      break;
+    case 'one':
+      handleDigitInput(1);
+      break;
+    case 'two':
+      handleDigitInput(2);
+      break;
+    case 'three':
+      handleDigitInput(3);
+      break;
+    case 'four':
+      handleDigitInput(4);
+      break;
+    case 'five':
+      handleDigitInput(5);
+      break;
+    case 'six':
+      handleDigitInput(6);
+      break;
+    case 'seven':
+      handleDigitInput(7);
+      break;
+    case 'eight':
+      handleDigitInput(8);
+      break;
+    case 'nine':
+      handleDigitInput(9);
+  }
+}
+
+const handleDigitInput = function (digit) {
+  
+}
+
+const clear = function () {
+
+}
+
+const digitButtons = document.querySelector('#digit-buttons');
+const utilityButtons = document.querySelector('#utility-buttons');
+
 let num1;
 let num2;
 let operator;
 
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
-
+digitButtons.addEventListener('click', handleDigitClick);
+utilityButtons.addEventListener('click', handleUtilityClick);
