8b80953e3ee0eb3ca61cef3ca1d530d99f634111
pattern-screamer <schuurmanlukas@gmail.com>
2026-02-09 15:30:04 +0100

    Create basic UI

---
+++ script.js
@@ -0,0 +1,14 @@
+let num1;
+let num2;
+let operator;
+
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+
